00c3b8cc6b22013acf590815e77f6c8405c6be60
Madhuri-Dilliker <madhurid@getreadyguilford.org>
2021-02-01 13:23:15 -0500

    Merge function with key as parameter

--- ProgramData.py
+++ ProgramData.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 14:27:12 2021
+
+@author: Madhuri
+"""
 #create a function to merge two dataset
 #1.loading the excel file
 #2.Loading dataset present in different sheets in dataframes
 #3.Define merge function
 #4.Call merge function
 #5.Testing the function using key used as parameter
+
 
 #importing python library
 import pandas as pd
@@ -16,22 +23,21 @@ program_df1 = program_df.parse(0)
 program_df2= program_df.parse(1)
 
 #defining the empty dataframe to store the merge data 
-final_program_df= pd.DataFrame()
+df_3= pd.DataFrame()
 
-#defining the id parameter
-id1='a013t000014wJIMAA2'
-id2='a013t000014wJIMAA2'
+
 
 #function to merge dataframes
-def merge_function (dataframe1,key1,dataframe2,key2):
-    if (key1== key2):  
-      final_program_df=pd.merge(dataframe1, dataframe2, how = 'left',left_on='Id1', right_on ='Id2')
-      print("merged dataframe", final_program_df)
-      return final_program_df
-
+def merge_function (df1,key1,df2,key2):
+      df_3 = df1.merge (df2, left_on = key1, right_on = key2)
+      print("merged dataframe", df_3)
+      return df_3
+  
 #calling the merge function
-final_program_df= merge_function(program_df1,id1,program_df2,id2)
+final_program_df1= merge_function(program_df1, 'Id1' , program_df2, 'Id2')
 
 #checking the merge data for given id
-data=final_program_df.loc[final_program_df['Id1']== id1]
-print("data", data)
+#data=final_program_df.loc[final_program_df['Id1']== id1]
+#print("data", data)
+
+final_program_df1.to_excel (r'C:\Users\Madhuri\Documents\GitHub\ProgramData\final_program_df1.xlsx', index = False, header=True)
