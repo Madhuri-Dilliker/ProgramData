dc69e0111a42faa89e1b41f3e5cb48f0f5e8acae
Madhuri-Dilliker <madhurid@getreadyguilford.org>
2021-02-01 17:03:30 -0500

    dataframe names are refined

--- ProgramData.py
+++ ProgramData.py
@@ -23,21 +23,18 @@ program_df1 = program_df.parse(0)
 program_df2= program_df.parse(1)
 
 #defining the empty dataframe to store the merge data 
-df_3= pd.DataFrame()
+program_df3= pd.DataFrame()
 
 
 
 #function to merge dataframes
 def merge_function (df1,key1,df2,key2):
-      df_3 = df1.merge (df2, left_on = key1, right_on = key2)
-      print("merged dataframe", df_3)
-      return df_3
+      program_df3 = df1.merge (df2, left_on = key1, right_on = key2)
+      print("merged dataframe", program_df3)
+      return program_df3
   
 #calling the merge function
-final_program_df1= merge_function(program_df1, 'Id1' , program_df2, 'Id2')
+final_program_df= merge_function(program_df1, 'Id1' , program_df2, 'Id2')
 
-#checking the merge data for given id
-#data=final_program_df.loc[final_program_df['Id1']== id1]
-#print("data", data)
-
-final_program_df1.to_excel (r'C:\Users\Madhuri\Documents\GitHub\ProgramData\final_program_df1.xlsx', index = False, header=True)
+#exporting data 
+final_program_df.to_excel (r'C:\Users\Madhuri\Documents\GitHub\ProgramData\final_program_df1.xlsx', index = False, header=True)
