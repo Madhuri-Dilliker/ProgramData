54c1d4597137f80e4387fcf1990e7616b28f1e05
Madhuri-Dilliker <madhurid@getreadyguilford.org>
2021-02-08 13:11:17 -0500

    Program Salesforce data

--- Spyder/Salesforce_Program_Data.py
+++ Spyder/Salesforce_Program_Data.py
@@ -13,13 +13,16 @@ Created on Wed Feb  3 15:37:39 2021
 #!pip install simple_salesforce
 import pandas as pd 
 from simple_salesforce import Salesforce
+import json
 
+with open(r'C:\Users\Madhuri\Documents\GitHub\ProgramData\Spyder\Credential.json') as Credential:
+     creds = json.load(Credential)
 #salesforce credentials
 sf = Salesforce(
-username='karthik@getreadyguilford.com', 
-password='RRDevOrg!156', 
-security_token='mOIMDBLw8w09HKqd69vQdSwO')
-
+username=creds['username'],
+password=creds['password'],             
+security_token=creds['token']
+)
 #soql query to extract data from child object
 SOQL="SELECT Date_of_Birth__c,First_Name__c,HSC_Gender__c,Last_Name__c,Test_number__c FROM Child__c"
 
